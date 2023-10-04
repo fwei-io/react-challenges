a9ab8a133fe981ccdc2e3d30949d437d47c84248
Wei Chung Chuo <wcchuo@yahoo.com>
2023-10-04 13:53:13 -0700

    Added GH icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,19 @@
 import React from "react";
+import GitHub from '../assets/icons/github-mark-white.svg'
 
 export default function Footer() {
     const currentYear = new Date(Date.now()).getFullYear();
     return (
         <footer id="footer" className="bg-teal-700 text-white text-xl">
             <section
-                className="max-w-4xl mx-auto p-4 flex flex-col sm:flex-row sm:justify-between"
+                className="max-w-6xl mx-auto p-4 flex flex-col sm:flex-row sm:justify-between"
             >
-                <address>
-                    <a href="https://github.com/fwei-io/react-challenges.git">
-                        <p className="text-sm">React Challenges</p>
-                    </a>
-                </address>
-
+                <a href="/">
+                    <p className="text-sm font-light">React Challenges | { currentYear }</p>
+                </a>
                 <div className="flex flex-col sm:gap-2">
-                    <a href="https://github.com/fwei-io/react-challenges.git">
-                        <p className="text-right text-sm">fwei-io { currentYear }</p>
+                    <a href="https://github.com/fwei-io/react-challenges.git" title="GitHub React Challenges">
+                        <img src={GitHub} alt="GitHub" className="w-5 h-5 flex" />
                     </a>
                 </div>
             </section>
